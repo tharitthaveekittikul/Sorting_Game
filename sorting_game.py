0c975a7f9819045e297596ec2198c0275b81d7e7
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2021-08-07 01:39:38 +0700

    Edit change_position

--- sorting_game.py
+++ sorting_game.py
@@ -5,56 +5,260 @@ class Input_processor:
     def __init__(self):
         self.charactor = " "
 
-    
     def Input_checker(self):
         while True:
             try:
-                self.charactor = input("Move: ")
-                if((self.charactor).upper() in ["A","B","C","D","E","F","G","H","I","J","K"]):
-                    print("OK")
+                self.charactor = input("Move: ").upper()
+                if((self.charactor) in ["A","B","C","D","E","F","G","H","I","J","K"]):
                     break
                 else:
                     print("Please put the charactor only A-K.")
 
             except:
                 print("Please put the charactor A-K only.")
+    
+    def get_charactor(self):
+        return self.charactor
 
 
-class Board(Input_processor):
+class Board:
 
     def __init__(self):
-        self.board = [["A","K","C","H"],
-                      ["D","E","B","I"],
-                      ["J","G","F"," "]] # main board
-        self.temp_board = [["A","K","C","H"],
-                      ["D","E","B","I"],
-                      ["J","G","F"," "]]
+        
+        self.charactor_list = ["A","B","C","D","E","F","G","H","I","J","K"," "]
+        self.cha_rd = rd.sample(self.charactor_list,12)
+        self.board = [["A","B","C","D"],
+                      ["E","F","G","H"],
+                      ["I","J","K"," "]] # main board
+        
+
+    def random_board(self):
+        for i in range(3):
+            for j in range(4):
+                self.board[i][j] = self.cha_rd[j-3]
+                self.cha_rd.remove(self.board[i][j])
+
+    def get_board(self):
+        return self.board
 
     def display_board(self):
-
-        b1 = rd.sample(range(3),3)
-        b2 = rd.sample(range(4),4)
-
-        for i in range(len(b1)):
-            for j in range(len(b2)):
-                self.board[i][j] = self.temp_board[b1[i]][b2[j]]
+        self.get_board()
+        for i in range(3):
+            for j in range(4):
                 sys.stdout.write(self.board[i][j] + "  ")
             print("\n")
-        # print("BOARD" , self.board)
 
     def check_winner(self):
         if(self.board == [["A","B","C","D"],["E","F","G","H"],["I","J","K"," "]]):
-            print("Win!")
+            return True
 
     def change_position(self):
-        i = Input_processor()
-        i.Input_checker()
+        while (True):
+            I = Input_processor()     
+            I.Input_checker()
+            gb = self.get_board()
 
+            for i in range(3):
+                for j in range(4):
+                    if(gb[i][j] == I.get_charactor()):
+                        index_char = [i,j]
 
+            # change 2 position
+            if(index_char == [0,0]):
+                if(gb[0][1] == " "):
+                    gb[0][0],gb[0][1] = gb[0][1],gb[0][0]
+                    break
+
+                elif(gb[1][0] == " " ):
+                    gb[0][0],gb[1][0] = gb[1][0],gb[0][0]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [0,3]):
+                if(gb[0][2] == " "):
+                    gb[0][3],gb[0][2] = gb[0][2],gb[0][3]
+                    break
+
+                elif(gb[1][3] == " "):
+                    gb[0][3],gb[1][3] = gb[1][3],gb[0][3]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+            
+            elif(index_char == [2,0]):
+                if(gb[1][0] == " "):
+                    gb[2][0],gb[1][0] = gb[1][0],gb[2][0]
+                    break
+                    
+                elif(gb[2][1] == " "):
+                    gb[2][0],gb[2][1] = gb[2][1],gb[2][0]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [2,3]):
+                if(gb[1][3] == " "):
+                    gb[2][3],gb[1][3] = gb[1][3],gb[2][3]
+                    break
+                    
+                elif(gb[2][2] == " "):
+                    gb[2][3],gb[2][2] = gb[2][2],gb[2][3]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            # change 3 position
+            elif(index_char == [0,1]):
+                if(gb[0][0] == " "):
+                    gb[0][1],gb[0][0] = gb[0][0],gb[0][1]
+                    break
+                    
+                elif(gb[1][1] == " "):
+                    gb[0][1],gb[1][1] = gb[1][1],gb[0][1]
+                    break
+
+                elif(gb[0][2] == " "):
+                    gb[0][1],gb[0][2] = gb[0][2],gb[0][1]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+            
+            elif(index_char == [0,2]):
+                if(gb[0][3] == " "):
+                    gb[0][2],gb[0][3] = gb[0][3],gb[0][2]
+                    break
+                    
+                elif(gb[0][1] == " "):
+                    gb[0][2],gb[0][1] = gb[0][1],gb[0][2]
+                    break
+
+                elif(gb[1][2] == " "):
+                    gb[0][2],gb[1][2] = gb[1][2],gb[0][2]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [1,0]):
+                if(gb[1][1] == " "):
+                    gb[1][0],gb[1][1] = gb[1][1],gb[1][0]
+                    break
+                    
+                elif(gb[0][0] == " "):
+                    gb[1][0],gb[0][0] = gb[0][0],gb[1][0]
+                    break
+
+                elif(gb[2][0] == " "):
+                    gb[1][0],gb[2][0] = gb[2][0],gb[1][0]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [1,3]):
+                if(gb[0][3] == " "):
+                    gb[1][3],gb[0][3] = gb[0][3],gb[1][3]
+                    break
+                    
+                elif(gb[1][2] == " "):
+                    gb[1][3],gb[1][2] = gb[1][2],gb[1][3]
+                    break
+
+                elif(gb[2][3] == " "):
+                    gb[1][3],gb[2][3] = gb[2][3],gb[1][3]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [2,1]):
+                if(gb[1][1] == " "):
+                    gb[2][1],gb[1][1] = gb[1][1],gb[2][1]
+                    break
+                    
+                elif(gb[2][0] == " "):
+                    gb[2][1],gb[2][0] = gb[2][0],gb[2][1]
+                    break
+
+                elif(gb[2][2] == " "):
+                    gb[2][1],gb[2][2] = gb[2][2],gb[2][1] 
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [2,2]):
+                if(gb[1][2] == " "):
+                    gb[2][2],gb[1][2] = gb[1][2],gb[2][2]
+                    break
+                    
+                elif(gb[2][1] == " "):
+                    gb[2][2],gb[2][1] = gb[2][1],gb[2][2]
+                    break
+
+                elif(gb[2][3] == " "):
+                    gb[2][2],gb[2][3] = gb[2][3],gb[2][2]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            # Change 4 position
+
+            elif(index_char == [1,1]):
+                if(gb[0][1] == " "):
+                    gb[1][1],gb[0][1] = gb[0][1],gb[1][1]
+                    break
+                    
+                elif(gb[1][0] == " "):
+                    gb[1][1],gb[1][0] = gb[1][0],gb[1][1]
+                    break
+
+                elif(gb[2][1] == " "):
+                    gb[1][1],gb[2][1] = gb[2][1],gb[1][1]
+                    break
+                
+                elif(gb[1][2] == " "):
+                    gb[1][1],gb[1][2] = gb[1][2],gb[1][1]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+
+            elif(index_char == [1,2]):
+                if(gb[0][2] == " "):
+                    gb[1][2],gb[0][2] = gb[0][2],gb[1][2]
+                    break
+                    
+                elif(gb[1][1] == " "):
+                    gb[1][2],gb[1][1] = gb[1][1],gb[1][2]
+                    break
+
+                elif(gb[2][2] == " "):
+                    gb[1][2],gb[2][2] = gb[2][2],gb[1][2]
+                    break
+                
+                elif(gb[1][3] == " "):
+                    gb[1][2],gb[1][3] = gb[1][3],gb[1][2]
+                    break
+
+                else:
+                    print("Cannot Change try again.")
+        
 if __name__ == '__main__':
     print("Welcome to Sorting Game")
     b = Board()
-    b.display_board()
-    b.change_position()
-    b.check_winner()
-
+    b.random_board()
+    while(True):
+        b.display_board()
+        b.change_position()
+        if (b.check_winner() == True):
+            b.display_board()
+            print("Win!!")
+            break
