85e31d95e00e99ae561a0cbfe5f1606b8888083a
tharitthaveekittikul <s6301012630079@email.kmutnb.ac.th>
2021-08-06 16:52:33 +0700

    change class position

--- sorting_game.py
+++ sorting_game.py
@@ -1,11 +1,32 @@
 import random as rd
 import sys
-class Board:
+class Input_processor:
 
     def __init__(self):
-        self.board = [["A","B","C","D"],["E","F","G","H"],["I","J","K"," "]]
+        self.charactor = " "
+
+    
+    def Input_checker(self):
+        try:
+            self.charactor = input("Move: ")
+            if((self.charactor).upper() in ["A","B","C","D","E","F","G","H","I","J","K"]):
+                print("OK")
+            else:
+                print("Please put the charactor only A-K.")
+
+        except:
+            print("Please put the charactor A-K only.")
+
+
+class Board(Input_processor):
+
+    def __init__(self):
+        self.board = [["A","K","C","H"],
+                      ["D","E","B","I"],
+                      ["J","G","F"," "]]
 
     def display_board(self):
+
         b1 = rd.sample(range(3),3)
         b2 = rd.sample(range(4),4)
 
@@ -14,18 +35,16 @@ class Board:
                 sys.stdout.write(self.board[b1[i]][b2[j]] + "  ")
             print("\n")
 
-class Input_processor(Board):
-
-    def __init__(self):
-        self.charactor = " "
-
-    
-    def Input_checker(self):
-        self.charactor = input("Move: ")
-    
+    def check_winner(self):
+        if(self.board == [["A","B","C","D"],["E","F","G","H"],["I","J","K"," "]]):
+            print("Win!")
 
 
 if __name__ == '__main__':
     print("Welcome to Sorting Game")
     b = Board()
-    print(b.display_board())
+    i = Input_processor()
+    b.display_board()
+    i.Input_checker()
+    b.check_winner()
+
